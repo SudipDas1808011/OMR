33031d9494889e784692f6d96d8e1ac538c97015
SUDIP DAS <55927107+SudipDas1808011@users.noreply.github.com>
2025-02-11 14:50:55 +0600

    Update script.js

--- script.js
+++ script.js
@@ -49,6 +49,7 @@ document.getElementById('omrForm').addEventListener('submit', function(event) {
 });
 
 document.getElementById('checkAnswersBtn').addEventListener('click', function() {
+    document.getElementById("submitbtn").style.display = 'show';
     const numQuestions = document.getElementById('numQuestions').value;
     const answersContainer = document.getElementById('answersContainer');
     const scoreContainer = document.getElementById('score');
